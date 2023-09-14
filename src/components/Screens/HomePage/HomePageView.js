79a10270c35d7599d893a8f54e18a7aebe50358f
giannesprokopos <giannisprokopos@gmail.com>
2023-09-14 20:01:01 +0300

    bottom bar initiated and paused for modal

--- src/components/Screens/HomePage/HomePageView.js
+++ src/components/Screens/HomePage/HomePageView.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Modal, StyleSheet, Text, Pressable } from 'react-native';
 import CardContainer from '../../card/CardContainer';
 import BottomBar from '../../bottomBar/BottomBar';
 
@@ -8,13 +8,86 @@ const flexAndColorSetter = (flex, backgroundColor) => {
 };
 
 export default function HomePageView() {
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
     <View style={{ flex: 1, flexDirection: 'column' }}>
+      <Modal
+        animationType='slide'
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello World!</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
       <View style={flexAndColorSetter(1, 'pink')} />
       <View style={flexAndColorSetter(8, 'pink')}>
+        <Pressable
+          style={[styles.button, styles.buttonOpen]}
+          onPress={() => setModalVisible(true)}
+        >
+          <Text style={styles.textStyle}>Show Modal</Text>
+        </Pressable>
         <CardContainer />
       </View>
       <BottomBar style={{ flex: 1 }} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+});
